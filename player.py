d6ee73e7b52d399e641bce45ce9c45495f9905ab
Tahia Hossain <tahia.hossain@g.bracu.ac.bd>
2023-12-08 20:37:11 +0600

    Completed Character Design

--- player.py
+++ player.py
@@ -53,5 +53,98 @@ class Player(GameObject):
                     self.y = collided_with.top + self.height
 
     def on_draw(self):
-        Draw.change_color("#FFFFFF")
-        Draw.rect(0, 0, self.width, self.height)
+        Draw.change_color("#ffc0cb")
+        Draw.circle(25,-20,-20)
+        Draw.circle(25,-20,-20)
+        Draw.circle(25,-20,-20)
+        
+        Draw.line(0,-20,-30,-60)
+        Draw.line(10,-20,40,-60)
+        Draw.line(-30,-60,40,-60)
+        # left eye
+        Draw.line(-2,16,-8,8)
+        Draw.line(-2,16,2,8)
+        # right eye
+        Draw.line(14,16,8,8)
+        Draw.line(14,16,18,8)
+        # left beard
+        Draw.line(2,-2,-30,10)
+        Draw.line(2,-4,-30,0)
+        Draw.line(2,-6,-30,-10)
+        # right beard
+        Draw.line(8,-2,40,10)
+        Draw.line(8,-4,40,0)
+        Draw.line(8,-6,40,-10)
+        # tail
+        Draw.line(-22,-45,-40,-40)
+        # nose
+        Draw.circle(5, 0, -10, fill=True)
+        # legs
+        Draw.circle(10, -20, -77, fill=True)
+        Draw.circle(10, 10, -77, fill=True)
+
+
+        # enemy 1   
+        Draw.change_color("#FF0000")
+        Draw.rect(75, 20, 45, 45)
+        Draw.rect(100, -25, 80, 40)
+        # legs
+        Draw.rect(115, -65, 15, 15, fill=True)
+        Draw.rect(150, -65, 15, 15, fill=True)
+        # tail
+        Draw.circle(7, 175, -30, fill=True)
+        # eyes
+        Draw.rect(82, 10, 10, 10, fill=True)
+        Draw.rect(100, 10, 10, 10, fill=True)
+        # mouth
+        Draw.rect(87, -10, 20, 5, fill=True)
+        # hair
+        Draw.line(75, 20, 90, 30)
+        Draw.line(85, 20, 100, 30)
+        Draw.line(95, 20, 110, 30)
+        Draw.line(105, 20, 120, 30)
+        Draw.line(115, 20, 130, 30)
+
+
+        # enemy 2
+        Draw.change_color("#FF00FF")
+        Draw.rect(212, 20, 45, 45)
+        Draw.rect(220, -25, 30, 40)
+        # leg
+        Draw.rect(210, -65, 50, 15, fill=True)
+        # eyes
+        Draw.rect(220, 10, 10, 10, fill=True)
+        Draw.rect(238, 10, 10, 10, fill=True)
+        # mouth
+        Draw.rect(225, -10, 20, 5, fill=True)
+        # hair
+        Draw.line(212, 20, 220, 50)
+        Draw.line(257, 20, 250, 50)
+        Draw.circle(5, 215, 50)
+        Draw.circle(5, 245, 50)
+        Draw.line(225, 20, 230, 40)
+        Draw.line(245, 20, 240, 40)
+        Draw.circle(3, 227, 40)
+        Draw.circle(3, 237, 40)
+
+
+        # enemy 3
+        Draw.change_color("#00FFFF")
+        Draw.rect(312, 20, 45, 45)
+        Draw.rect(320, -25, 30, 40)
+        # leg
+        Draw.rect(310, -65, 50, 15, fill=True)
+        # eyes
+        Draw.rect(320, 10, 10, 10, fill=True)
+        Draw.rect(338, 10, 10, 10, fill=True)
+        # mouth
+        Draw.rect(325, -10, 20, 5, fill=True)
+        # hair
+        Draw.line(312, 20, 320, 50)
+        Draw.line(325, 20, 320, 50)
+        Draw.line(325, 20, 330, 50)
+        Draw.line(338, 20, 330, 50)
+        Draw.line(338, 20, 345, 50)
+        Draw.line(350, 20, 345, 50)
+        Draw.line(350, 20, 355, 50)
+        Draw.line(358, 20, 355, 50)
